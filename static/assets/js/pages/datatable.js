744f6b376446896290e635e8fd69cdaafad8b493
Joshua Parker <josh@joshparker.name>
2018-06-28 20:46:07 -0400

    Datatable fix for db sort filter.

--- static/assets/js/pages/datatable.js
+++ static/assets/js/pages/datatable.js
@@ -1,5 +1,14 @@
 $(function () {
-    $("#example1").DataTable();
+    $("#example1").DataTable({
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "aaSorting": [],
+        "ordering": true,
+        "info": true,
+        "autoWidth": true,
+        "deferRender": true
+    });
     $('#example2').DataTable({
         "paging": true,
         "lengthChange": false,
